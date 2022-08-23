572458ca753dec1b3897d3e2f707f648257480fe
Sampsa Vaurio <sampsa.vaurio@gmail.com>
2022-08-23 14:56:28 +0300

    add index viewmodel, add DB connections

--- Models/UserService.cs
+++ Models/UserService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Graph;
 
 
 namespace active_directory_aspnetcore_webapp_openidconnect_v2.Models
@@ -16,12 +17,43 @@ namespace active_directory_aspnetcore_webapp_openidconnect_v2.Models
             _DbContext = context;
         }
 
-        public async Task<bool> CheckUser(string email)
+        public async Task<bool> CheckUserExistDbAsync(string email)
         {
-            UserService dbUser = await _DbContext.UserData.FindAsync.Where(x => x.Email == email).FirstOrDefaultAsync();
-            dbUser.
-            await Task.Delay(1000);
+            UserInformation dbUser = await _DbContext.UserData.FirstOrDefaultAsync(x => x.Email == email);
 
+            if (dbUser == null)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public async Task AddUserToDb(string email) 
+        {
+            bool userExcists = await CheckUserExistDbAsync(email);
+            if (!userExcists)
+            {
+                UserInformation newUser = new UserInformation()
+                {
+                    Email = email,
+                    HasUploadedData = false
+                };
+
+                _DbContext.UserData.Add(newUser);
+                await _DbContext.SaveChangesAsync();
+            }
+            else
+            {
+                throw new ArgumentException($"User found with email {email}");
+            }
+        }
+
+        public async Task<IndexViewModel> CreateIndexViewModel(User userFromAzureAD) 
+        {
+            bool userExcists = await CheckUserExistDbAsync(userFromAzureAD.Mail);
+            IndexViewModel newIndexViewModel = new IndexViewModel(userFromAzureAD, userExcists);
+
+            return newIndexViewModel;
         }
     }
 }
